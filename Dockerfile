5d7a7314100a7634ac6b665c7f0da1d0f0dc25a8
Saumya8 <saumyagangrade8@gmail.com>
2024-02-16 11:44:06 -0500

    lab 5 done

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,20 @@
 FROM python:3.9-slim
 
 # TODO [1]: Set WORKDIR to /app
+WORKDIR /app
 
 # TODO [2.1]: Copy the requirements.txt file to the WORKDIR
+COPY requirements.txt /app/
+
+# Deliverable 3: Include trained model
+COPY iris_model.pkl /app/
 
 # TODO [2.2]: Install the requirements
+RUN pip install --no-cache-dir -r requirements.txt
 
 # TODO [3]: Copy the server to the WORKDIR
+COPY . /app/
 
 # TODO [4]: Set the command to run the app
-CMD [...]
+CMD ["python", "server.py"]
+#CMD [...]
--- server.py
+++ server.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import numpy as np
 import joblib
 
 app = Flask(__name__)
+
+# TODO: Import trained model
+MODEL_PATH = 'iris_model.pkl'
+
+# Load the model at the start of the app to avoid loading it for each request
+model = joblib.load(MODEL_PATH)  
 
 from flask import request
 
@@ -12,15 +18,20 @@ def predict():
     get_json = request.get_json()
     iris_input = get_json['input']
     
-    # TODO: Import trained model
-    model = ...
+    #model = MODEL_PATH
     
     # TODO: Make prediction using the model 
     # HINT: use np.array().reshape(1, -1) to convert input to 2D array
-    prediction = ...
+    # Convert input to 2D array as the model expects 2D array inputs
+    input_array = np.array(iris_input).reshape(1, -1)
+    
+    # Make prediction using the model
+    prediction = model.predict(input_array)
+    #prediction = ...
     
     # TODO: Return the prediction as a response
-    return ...
+    #return ...
+    return jsonify({'prediction': prediction.tolist()})
 
 @app.route('/')
 def hello():
